10ec2fddd0061280506dbde95d51d9301272f7f2
mkang315 <53328998+mkang315@users.noreply.github.com>
2023-05-26 11:02:34 +0800

    Update gene_txt.py

--- dataset-Br35H/gene_txt.py
+++ dataset-Br35H/gene_txt.py
@@ -4,4 +4,4 @@ jpgs = glob.glob('./*/*.jpg')[120:]
 
 for x in jpgs:
     with open('train.txt', 'a') as f:
-        f.write('dataBr35H/' + x[2:] + '\n')
+        f.write('dataset-Br35H/' + x[2:] + '\n')
