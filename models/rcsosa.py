0c931423df3321c3d29d0342f8fb46c397a14edb
mkang315 <53328998+mkang315@users.noreply.github.com>
2023-05-29 19:06:43 +0800

    Add files via upload

---
+++ models/rcsosa.py
@@ -0,0 +1,195 @@
+from models.common import Conv
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+import numpy as np
+import math
+
+# build RepVGG block
+# -----------------------------
+def conv_bn(in_channels, out_channels, kernel_size, stride, padding, groups=1):
+    result = nn.Sequential()
+    result.add_module('conv', nn.Conv2d(in_channels=in_channels, out_channels=out_channels,
+                                        kernel_size=kernel_size, stride=stride, padding=padding, groups=groups,
+                                        bias=False))
+    result.add_module('bn', nn.BatchNorm2d(num_features=out_channels))
+
+    return result
+
+class SEBlock(nn.Module):
+    def __init__(self, input_channels):
+        super(SEBlock, self).__init__()
+        internal_neurons = input_channels // 8
+        self.down = nn.Conv2d(in_channels=input_channels, out_channels=internal_neurons, kernel_size=1, stride=1,
+                              bias=True)
+        self.up = nn.Conv2d(in_channels=internal_neurons, out_channels=input_channels, kernel_size=1, stride=1,
+                            bias=True)
+        self.input_channels = input_channels
+
+    def forward(self, inputs):
+        x = F.avg_pool2d(inputs, kernel_size=inputs.size(3))
+        x = self.down(x)
+        x = F.relu(x)
+        x = self.up(x)
+        x = torch.sigmoid(x)
+        x = x.view(-1, self.input_channels, 1, 1)
+        return inputs * x
+
+class RepVGG(nn.Module):
+
+    def __init__(self, in_channels, out_channels, kernel_size=3,
+                 stride=1, padding=1, dilation=1, groups=1, padding_mode='zeros', deploy=False, use_se=False):
+        super(RepVGG, self).__init__()
+        self.deploy = deploy
+        self.groups = groups
+        self.in_channels = in_channels
+
+        padding_11 = padding - kernel_size // 2
+
+        self.nonlinearity = nn.SiLU()
+        # self.nonlinearity = nn.ReLU()
+
+        if use_se:
+            self.se = SEBlock(out_channels, internal_neurons=out_channels // 16)
+        else:
+            self.se = nn.Identity()
+
+        if deploy:
+            self.rbr_reparam = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                         stride=stride,
+                                         padding=padding, dilation=dilation, groups=groups, bias=True,
+                                         padding_mode=padding_mode)
+
+        else:
+            self.rbr_identity = nn.BatchNorm2d(
+                num_features=in_channels) if out_channels == in_channels and stride == 1 else None
+            self.rbr_dense = conv_bn(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size,
+                                     stride=stride, padding=padding, groups=groups)
+            self.rbr_1x1 = conv_bn(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride,
+                                   padding=padding_11, groups=groups)
+            # print('RepVGG Block, identity = ', self.rbr_identity)
+
+    def get_equivalent_kernel_bias(self):
+        kernel3x3, bias3x3 = self._fuse_bn_tensor(self.rbr_dense)
+        kernel1x1, bias1x1 = self._fuse_bn_tensor(self.rbr_1x1)
+        kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity)
+        return kernel3x3 + self._pad_1x1_to_3x3_tensor(kernel1x1) + kernelid, bias3x3 + bias1x1 + biasid
+
+    def _pad_1x1_to_3x3_tensor(self, kernel1x1):
+        if kernel1x1 is None:
+            return 0
+        else:
+            return torch.nn.functional.pad(kernel1x1, [1, 1, 1, 1])
+
+    def _fuse_bn_tensor(self, branch):
+        if branch is None:
+            return 0, 0
+        if isinstance(branch, nn.Sequential):
+            kernel = branch.conv.weight
+            running_mean = branch.bn.running_mean
+            running_var = branch.bn.running_var
+            gamma = branch.bn.weight
+            beta = branch.bn.bias
+            eps = branch.bn.eps
+        else:
+            assert isinstance(branch, nn.BatchNorm2d)
+            if not hasattr(self, 'id_tensor'):
+                input_dim = self.in_channels // self.groups
+                kernel_value = np.zeros((self.in_channels, input_dim, 3, 3), dtype=np.float32)
+                for i in range(self.in_channels):
+                    kernel_value[i, i % input_dim, 1, 1] = 1
+                self.id_tensor = torch.from_numpy(kernel_value).to(branch.weight.device)
+            kernel = self.id_tensor
+            running_mean = branch.running_mean
+            running_var = branch.running_var
+            gamma = branch.weight
+            beta = branch.bias
+            eps = branch.eps
+        std = (running_var + eps).sqrt()
+        t = (gamma / std).reshape(-1, 1, 1, 1)
+        return kernel * t, beta - running_mean * gamma / std
+
+    def forward(self, inputs):
+        if hasattr(self, 'rbr_reparam'):
+            return self.nonlinearity(self.se(self.rbr_reparam(inputs)))
+
+        if self.rbr_identity is None:
+            id_out = 0
+        else:
+            id_out = self.rbr_identity(inputs)
+
+        return self.nonlinearity(self.se(self.rbr_dense(inputs) + self.rbr_1x1(inputs) + id_out))
+
+    def fusevggforward(self, x):
+        return self.nonlinearity(self.rbr_dense(x))
+
+# RepVGG block end
+# -----------------------------
+
+class SR(nn.Module):
+    # Shuffle RepVGG
+    def __init__(self, c1, c2):
+        super().__init__()
+        c1_ = int(c1 // 2)
+        c2_ = int(c2 // 2)
+        self.repconv = RepVGG(c1_, c2_)
+
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        out = torch.cat((x1, self.repconv(x2)), dim=1)
+        out = self.channel_shuffle(out, 2)
+        return out
+
+    def channel_shuffle(self, x, groups):
+        batchsize, num_channels, height, width = x.data.size()
+        channels_per_group = num_channels // groups
+        x = x.view(batchsize, groups, channels_per_group, height, width)
+        x = torch.transpose(x, 1, 2).contiguous()
+        x = x.view(batchsize, -1, height, width)
+        return x
+
+class ResSR(nn.Module):
+    # Res Shuffle RepVGG
+    def __init__(self, c, shortcut=False):
+        super().__init__()
+        self.repconv1 = RepVGG(c, c)
+        self.repconv2 = RepVGG(c, c)
+        self.shortcut = shortcut
+
+    def forward(self, x):
+        return (x + self.repconv2(self.repconv1(x))) if self.shortcut else self.repconv2(self.repconv1(x))
+
+def make_divisible(x, divisor):
+    # Returns nearest x divisible by divisor
+    if isinstance(divisor, torch.Tensor):
+        divisor = int(divisor.max())  # to int
+    return math.ceil(x / divisor) * divisor
+
+class RCSOSA(nn.Module):
+    # VoVNet with Res Shuffle RepVGG
+    def __init__(self, c1, c2, n=1, se=False, e=0.5, stackrep=True):
+        super().__init__()
+        n_ = n // 2
+        c_ = make_divisible(int(c1 * e), 8)
+        # self.conv1 = Conv(c1, c_)
+        self.conv1 = RepVGG(c1, c_)
+        self.conv3 = RepVGG(int(c_ * 3), c2)
+        self.sr1 = nn.Sequential(*[SR(c_, c_) for _ in range(n_)])
+        self.sr2 = nn.Sequential(*[SR(c_, c_) for _ in range(n_)])
+
+        self.se = None
+        if se:
+            self.se = SEBlock(c2)
+
+    def forward(self, x):
+        x1 = self.conv1(x)
+        x2 = self.sr1(x1)
+        x3 = self.sr2(x2)
+        x = torch.cat((x1, x2, x3), 1)
+        return self.conv3(x) if self.se is None else self.se(self.conv3(x))
+
+if __name__ == '__main__':
+    m = RCSOSA(256, 256)
+    im = torch.randn(2, 256, 13, 13)
+    y = m(im)
+    print(y.shape)
